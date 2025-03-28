6bd2f116ed1ee43d7abe76f14a8de604ea095a1b
Ashish Autade <ashishautade1@gmail.com>
2025-03-28 17:15:32 +0530

    product added ashish

---
+++ Models/Product.cs
@@ -0,0 +1,9 @@
+﻿namespace Test.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public string?  ProductName { get; set; }
+        public string? ProductDescription { get; set; }
+    }
+}
